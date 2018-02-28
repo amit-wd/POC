eed0e09a4cf81872e42ca51f6569dbaf09c6544c
Andrew Crites <acrites@mobiquityinc.com>
2018-02-28 16:12:05 -0500

    fix(training): use correct test for training

--- src/__tests__/Hello.test.tsx
+++ src/__tests__/Hello.test.tsx
@@ -1,11 +1,11 @@
 import 'react-native';
 import * as React from 'react';
-import { App } from 'src/App';
+import { Hello } from 'src/Hello';
 
 // Note: test renderer must be required after react-native.
 import { create } from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = create(<App />);
+  const tree = create(<Hello />);
   tree;
 });
